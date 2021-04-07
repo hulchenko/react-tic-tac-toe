219559bf73727f358f6ab54bed541e6a4192dd8f
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-04-07 12:51:20 -0600

    add changes

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,13 +4,13 @@ import './index.css';
 
 class Square extends React.Component {
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return <button className="square">{this.props.value /* #2 */}</button>;
   }
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} /* #1 */ />;
   }
 
   render() {
